f2140f97712a0ec3cedb860b8eeabcfb8370f9a7
season bhattarai <bhattaraiseason22@gmail.com>
2023-04-24 18:00:39 +0545

    progress on dashboard

--- app/src/main/java/com/generic/wallet/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/generic/wallet/dashboard/DashboardFragment.kt
@@ -1,11 +1,28 @@
 package com.generic.wallet.dashboard
 
+import android.content.Context
+import android.content.res.Resources
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.LinearLayout.LayoutParams
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.setMargins
+import androidx.core.view.setPadding
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.generic.wallet.R
+import com.generic.wallet.databinding.FragmentDashboardBinding
+import org.w3c.dom.Text
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +38,9 @@ class DashboardFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentDashboardBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +55,81 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val array = listOf(
+            R.drawable.send_money,
+            R.drawable.request_money,
+            R.drawable.load_wallet,
+            R.drawable.bank_transfer_01
+        )
+
+        val linearLayout = binding.servicesHorizontalLayout
+
+        linearLayout.apply {
+            orientation = LinearLayout.HORIZONTAL
+            array.forEach {
+                val imageView = ImageView(requireContext())
+
+                imageView.layoutParams = LayoutParams(
+                    0,
+                    LayoutParams.WRAP_CONTENT, 1.0f
+                ).apply {
+                    setMargins(50, 16, 12, 50)
+                }
+
+                imageView.setPadding(20, 40, 20, 40)
+                imageView.setBackgroundResource(R.drawable.wallet_corner_radius)
+
+                imageView.setImageResource(it)
+                addView(imageView)
+            }
+            gravity = Gravity.CENTER_VERTICAL
+
+
+            (layoutParams as ConstraintLayout.LayoutParams).setMargins(
+                16, 50, 16, 0
+            )
+        }
+
+        class ViewHolder(val textView:TextView): RecyclerView.ViewHolder(textView) {
+            fun setText(text:String){
+                textView.text = text
+            }
+
+        }
+
+        val items = (1..100).map {
+            "Hello $it"
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+
+        binding.recyclerView.adapter = object : RecyclerView.Adapter<ViewHolder>() {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+                return ViewHolder(
+                    TextView(context)
+                )
+            }
+
+            override fun getItemCount(): Int {
+               return items.size
+            }
+
+            override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+                items[position].let {
+                    holder.setText(it)
+                }
+            }
+
+        }
+
+
     }
 
     companion object {
@@ -57,4 +151,9 @@ class DashboardFragment : Fragment() {
                 }
             }
     }
+
+
+
 }
+
+
